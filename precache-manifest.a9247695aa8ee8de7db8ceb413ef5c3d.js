b1d9abfa7f78f8738a5327a8bed9341671502844
Finnik723 <hydrosanek@google.com>
2021-02-05 01:45:06 +0300

    Initial commit

---
+++ precache-manifest.a9247695aa8ee8de7db8ceb413ef5c3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94429e506d4b73ce9b922816db57da8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06c31b29fb6941a7706d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a6c3e51b35eef04b9145",
+    "url": "/static/css/main.f3021772.chunk.css"
+  },
+  {
+    "revision": "06c31b29fb6941a7706d",
+    "url": "/static/js/2.d0f004b1.chunk.js"
+  },
+  {
+    "revision": "c557bf33e3652612926eab19870df083",
+    "url": "/static/js/2.d0f004b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c3e51b35eef04b9145",
+    "url": "/static/js/main.059b1c6d.chunk.js"
+  },
+  {
+    "revision": "d1373d0c8b49a7696917",
+    "url": "/static/js/runtime-main.367c835c.js"
+  }
+]);
